d0b60384aededeae3ad8305fa542b858c4320907
omarchouman <omar.chouman0@gmail.com>
2022-02-01 20:30:32 +0200

    Working on user actions & jwt middleware

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,12 @@
-const router = require('express').Router();
+const { updateUser } = require("../controllers/UserController");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+} = require("../middlewares/verifyToken");
 
+const router = require("express").Router();
+
+//UPDATE USER
+router.put("/:id", verifyTokenAndAuthorization, updateUser);
 
 module.exports = router;
